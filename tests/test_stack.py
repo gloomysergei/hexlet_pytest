aa39f8d816a5daf5c90cb3667f79c32580ee7cae
gloomysergei <almazopal@gmail.com>
2026-01-14 10:24:52 +0300

    feat: modular testing section

---
+++ tests/test_stack.py
@@ -0,0 +1,22 @@
+import pytest
+from hexlet_pytest.stack import stack
+
+def test_stack():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+    
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)
+    stack.pop()
+    assert not stack
+    
+def test_pop_with_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
